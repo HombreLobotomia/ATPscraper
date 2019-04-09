35b7491074be2a3c930b05c4b1d621a9b415d693
HombreLobotomia <49406857+HombreLobotomia@users.noreply.github.com>
2019-04-09 18:45:29 +0200

    Update ATPscraper_reloaded.py

--- ATPscraper_reloaded.py
+++ ATPscraper_reloaded.py
@@ -11,7 +11,7 @@ import re
 #usate da pagina del torneo, esempio: https://www.atptour.com/en/scores/archive/doha/451/2019/results
 
 def matchScraper(url):
-    
+    match_ID = url.split('/')[-2]
     site = urlopen(Request(url, headers=hdr))
     listaframe = pd.read_html(site)
     score = listaframe[0].dropna(1, thresh=2).dropna(0).set_index(0)
@@ -27,13 +27,15 @@ def matchScraper(url):
     else:
                 dt_rows = dt_rows[1]+dt_rows[0]
 
-    df2[gen[i]]=df2[gen[i]]+dt_rows
+    df2[gen[i]]=df2[gen[i]]+dt_rows+match_ID
     #dt = dt.set_index(score.index)
     
     #dt = score.join(dt)
     #dt.to_csv('laparta.csv', mode='a')
     
 url = input('url')
+split_url = url.split('/')
+
 
 reqtext = requests.get(url).text
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -42,7 +44,9 @@ urlspattern = re.compile('/en/scores/\d{4}.+match.+False')
 path_list = re.findall(urlspattern, reqtext)
 domain_name = 'https://www.atptour.com'
 url_list = [domain_name+path for path in path_list]
-
+tourney_year = split_url[-2]
+tourney_ID = split_url[-3]
+tourney_name = split_url[-4]
 listaframe = pd.read_html(site)
 df=listaframe[2]
 df = df.drop(['Unnamed: 3','Unnamed: 8', 'Unnamed: 9', 'Unnamed: 1', 'Unnamed: 5'], axis=1)
@@ -65,5 +69,5 @@ if len(url_list)>0:
        'Service Games Played', 'Return Rating', '1st Serve Return Points Won',
        '2nd Serve Return Points Won', 'Break Points Converted',
        'Return Games Played', 'Service Points Won', 'Return Points Won',
-       'Total Points Won']
+       'Total Points Won', 'Match ID']
 final_d.to_csv('torneo.csv')
